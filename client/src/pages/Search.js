d052b27d309c4b9479d4051744fc513a40b22d91
Ayla Dillis <aayladillis@gmail.com>
2020-08-14 14:54:58 -0700

    renaming inputSearch

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,7 +3,7 @@ import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
-import inputSearch from "../components/SearchForm";
+import inputSearch from "../components/inputSearch";
 
 const Search = () => {
     return (
