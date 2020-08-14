af9fb1eb9a1d638b097c39fdfdd19d64fc093342
Sierra Chapman <siechap@gmail.com>
2020-08-14 11:39:54 -0700

    edit action strings

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,6 +1,5 @@
 export const LOADING="LOADING";
 export const UPDATE_RESULTS = "UPDATE_RESULTS";
-export const UPDATE_FAVORITES = "UPDATE_FAVORITES";
-export const ADD_FAVORITE = "ADD_FAVORITE";
-export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
-export const UPDATE_FAVORITES = "UPDATE_FAVORITES";
+export const UPDATE_BOOKS = "UPDATE_BOOKS";
+export const ADD_BOOK = "ADD_BOOK";
+export const REMOVE_BOOK = "REMOVE_BOOK";
