1683f7b6d88beaec506cf8b5ea6d98247b4010f1
Sierra Chapman <siechap@gmail.com>
2020-08-14 13:33:29 -0700

    fix error with router import

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import Search from "./pages/Search"
-import { BrowserRouter as Route, Switch, Router} from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <Router>
-      <Switch>
-      <Route exact path="/" component={Search} />
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Search} />
+        </Switch>
+      </div>
     </Router>
   );
 }
