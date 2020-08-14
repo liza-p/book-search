b8858ed08a4bfc1d4fdb644834294f6a8a258d37
Ayla Dillis <aayladillis@gmail.com>
2020-08-14 14:59:22 -0700

    bugs

--- client/src/components/inputSearch.js
+++ client/src/components/inputSearch.js
@@ -4,14 +4,14 @@ import React from "react";
 function inputSearch() {
     return (
         <div className="card">
-        <div className="card-body">
-        <div className="input-group mb-3">
-        <div className="input-group-prepend">
-            <span className="input-group-text" id="inputGroup-sizing-default">Search</span>
-        </div>
-        <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
-        </div>
-        </div>
+            <div className="card-body">
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Search</span>
+                    </div>
+                <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                </div>
+            </div>
         </div>
     )
 }
