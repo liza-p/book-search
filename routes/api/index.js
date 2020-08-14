ccdde49f22ae4b806c86302fe47774f34b1f5cc9
Ayla Dillis <aayladillis@gmail.com>
2020-08-14 12:40:24 -0700

    fixing books route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const postRoutes = require("./books");
+const bookRoutes = require("./books");
 
 // Books routes
-router.use("/books", postRoutes);
+router.use("/books", bookRoutes);
 
 module.exports = router;
