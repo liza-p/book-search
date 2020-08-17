5a123811866ddf04b8bc6e105ee01fac466d70a8
Sierra Chapman <siechap@gmail.com>
2020-08-17 12:31:43 -0700

    add BookProvider to wrap everything

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import axios from 'axios';
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
+import { BookProvider } from "./utils/GlobalState";
 import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
+        <BookProvider>
         <Switch>
           <Redirect exact path="/" to="/search" />
           <Route exact path="/search" component={Search} />
           <Route exact path="/saved" component={Saved} />
         </Switch>
+        </BookProvider>
       </div>
     </Router>
   );
