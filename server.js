b2fc8176dfe4fd58c1ef873081663df9151ea070
Sierra Chapman <siechap@gmail.com>
2020-08-17 10:33:14 -0700

    fix API route to search Google Books

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
 
 // Define middleware here
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
