8900d92f726c2e2e4da6d263bc44d29962f0cc8f
liza p <liyourlp@gmail.com>
2020-08-17 19:17:25 -0700

    add colors to Googlebooks tab

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Jumbotron.css";
 
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 400, clear: "both", paddingTop: 120, textAlign: "center"}}
+      style={{ height: 400, clear: "both", paddingTop: 290, textAlign: "center"}}
       className="jumbotron border border-dark"
     >
       {children}
-      <h1> (React) Google Books Search</h1>
-      <h2>Search for and Save Books of Interest</h2>
+      {/* <h3> (React) Google Books Search</h3> */}
+      <h3>Search and Save Books of Interest</h3>
 
     </div>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar({ children }) {
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <a className="navbar-brand" href="https://books.google.com/">Google Books</a>
+  <a className="navbar-brand" href="https://books.google.com/"><span className="text-primary">G</span><span className="text-danger">o</span><span className="text-warning">o</span><span className="text-primary">g</span><span className="text-success">l</span><span className="text-danger">e</span> <span  className="text-primary"> Books</span></a>
 
   <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
